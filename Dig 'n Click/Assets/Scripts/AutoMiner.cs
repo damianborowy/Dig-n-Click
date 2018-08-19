d96ded70ad8582e7145a98d6d44102fb025a7ce8
Unknown <tymooteuusz@gmail.com>
2018-08-19 13:19:03 +0200

    Updated AutoMiner and GameController

--- Dig 'n Click/Assets/Scripts/AutoMiner.cs
+++ Dig 'n Click/Assets/Scripts/AutoMiner.cs
@@ -1,7 +1,19 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoMiner : MonoBehaviour
 {
+    private void Start()
+    {
+        StartCoroutine(AutoMine());
+    }
+
+    IEnumerator AutoMine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            GameController.Instance.Money += 1;
+        }
+    }
 }
--- Dig 'n Click/Assets/Scripts/GameController.cs
+++ Dig 'n Click/Assets/Scripts/GameController.cs
@@ -4,4 +4,25 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController Instance { get; set; }
+    public double Money { get; set; }
+    public UnityEngine.UI.Text _moneyDisplay;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        _moneyDisplay.text = "Money: " + Money;
+    }
 }
