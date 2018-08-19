8487cf4a332109a892cfad0ea1e2c8b0a16fa725
Paweł Cembaluk <241574@student.pwr.edu.pl>
2018-08-19 22:59:22 +0200

    First day commit.

--- Dig 'n Click/Assets/Scripts/GameController.cs
+++ Dig 'n Click/Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,29 +8,40 @@ public class GameController : MonoBehaviour
     public static GameController Instance;
     public Text MoneyDisplay;
     public Text LevelDisplay;
+    public GameObject[] Rocks;
 
     private double _money;
     private int _level = 1;
+    private Vector3 _rockSpawn;
 
     public void AddMoney(double added)
     {
         _money += added;
     }
+
     public void SubMoney(double subbed)
     {
         _money -= subbed;
     }
+
     public double GetMoney()
     {
         return _money;
     }
+
     public int GetLevel()
     {
         return _level;
     }
+
     public void SetLevel(int level)
     {
         this._level = level;
+    }
+
+    public void SpawnRock()
+    {
+        Instantiate(Rocks[Random.Range(0, Rocks.Length)], _rockSpawn, Quaternion.identity);
     }
 
     private void Awake()
@@ -45,9 +57,21 @@ public class GameController : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        SetRockSpawn();
+        SpawnRock();
+    }
+
     private void Update()
     {
         MoneyDisplay.text = "Money: " + _money;
         LevelDisplay.text = "Level: " + _level;
+    }
+
+    private void SetRockSpawn()
+    {
+        double worldScreenHeight = Camera.main.orthographicSize * 2.0;
+        _rockSpawn = new Vector3(0, (float) worldScreenHeight, 0);
     }
 }
--- Dig 'n Click/Assets/Scripts/Rock/RockController.cs
+++ Dig 'n Click/Assets/Scripts/Rock/RockController.cs
@@ -1,32 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RockController : MonoBehaviour {
+public class RockController : MonoBehaviour
+{
+    public float InitialFallingSpeed;
+    public Sprite[] Sprites;
 
-    public Text HealthDisplay;
-    public float Health;
-    public double Reward;
+    private Slider _slider;
+    private Text _hpLeftDisplay;
+    private float _health;
+    private float _maxHealth;
+    private double _reward;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
 
     private void Start()
     {
+        _slider = GameObject.FindGameObjectWithTag("Slider").GetComponent<Slider>();
+        _hpLeftDisplay = _slider.GetComponentInChildren<Text>();
+
         int level = GameController.Instance.GetLevel();
-        Health = 10 * Mathf.Pow(1.1f, level) - 1;
-        Reward = Random.Range(1, 10) * level;
+        _health = 10 * Mathf.Pow(1.1f, level) - 1;
+        _maxHealth = _health;
+        _reward = Random.Range(1, 10) * level;
+
+        _rb = GetComponent<Rigidbody2D>();
+        _rb.velocity = new Vector2(0, -InitialFallingSpeed);
+
+        _sr = GetComponent<SpriteRenderer>();
+        _sr.sprite = Sprites[0];
     }
 
     public void Hit(float strength)
     {
-        if (strength < Health)
+        if (strength < _health)
         {
-            Health -= strength;
+            _health -= strength;
+
+            if (_health < _maxHealth / 4)
+                _sr.sprite = Sprites[3];
+            else if (_health < _maxHealth / 2)
+                _sr.sprite = Sprites[2];
+            else if (_health < _maxHealth * 3 / 4)
+                _sr.sprite = Sprites[1];
+
+                Debug.Log("Rock's Health: " + _health);
         }
-        
+        else
+        {
+            Debug.Log("Rock destroyed");
+
+            GameController.Instance.AddMoney(_reward);
+            GameController.Instance.SpawnRock();
+            Destroy(gameObject);
+        }
     }
 
     private void Update()
     {
-        HealthDisplay.text = "Rock's Health: " + Health;
+        _slider.value = _health / _maxHealth;
+        _hpLeftDisplay.text = _health + " / " + _maxHealth;
     }
 }
